964ec8205590874e5f3ad2487d7aae559c7a36af
Miss-Hacking-888 <137291891+Miss-Hacking-888@users.noreply.github.com>
2023-08-19 17:25:30 +0300

    Add files via upload

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -16,7 +16,7 @@ class Advertisement (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User,verbose_name='Пользователь',on_delete=models.CASCADE)
-    image = models.ImageField('Изображение', upload_to='advertisements/')
+    image = models.ImageField('Изображение', upload_to='advertisements/',null=True,blank=True)
 
 
 
@@ -40,6 +40,13 @@ class Advertisement (models.Model):
                 '<span style="color: green; font-weidht: bold;">Сегодня в {} </span>', created_time
             )
         return self.updated_at
+    
+    @admin.display (description='Фото')
+    def get_html_image (self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height:80px;"', url=self.image.url
+            )
 
     def __str__(self):
          return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
